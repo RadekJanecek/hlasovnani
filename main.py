4d7884c4c1ac76b8f14cf9271cec8e2d3513a4ae
Radek Janeček <83546285+RadekJanecek@users.noreply.github.com>
2022-04-20 19:32:36 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
+radio.set_group(1)
+radio.set_transmit_power(7)
+radio.set_transmit_serial_number(True)
 abeceda = ["A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "U", "V", "W", "X", "Y", "Z"]
-písmeno = abeceda[0]
-basic.show_string(písmeno)
-def on_forever():
-    
-    def on_button_pressed_a():
-        písmeno = abeceda[-1]
+pismeno = 0
+datalist = [0]
+start = False
+basic.show_string(abeceda[pismeno])
+
+def on_pin_pressed_p0():
+    global start
+    if start:
+        start = False
+        print("konec")
+    else:
+        start = True
+        print(start)
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+
+def on_button_pressed_a():
+    global pismeno
+    if pismeno > 0:
+        pismeno -= 1
         basic.clear_screen()
-        basic.show_string(písmeno)
-    input.on_button_pressed(Button.A, on_button_pressed_a)
-    def on_button_pressed_b():
-        písmeno = abeceda[+1]
+        basic.show_string(abeceda[pismeno])
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    global pismeno
+    if pismeno < len(abeceda):
+        pismeno += 1
         basic.clear_screen()
-        basic.show_string(písmeno)
-    input.on_button_pressed(Button.B, on_button_pressed_b)
-basic.forever(on_forever)
+        basic.show_string(abeceda[pismeno])
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_event_pressed():
-    radio.send_string(písmeno)
+    radio.send_string(abeceda[pismeno])
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+def on_received_string(receivedString):
+    odpoved = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    if start:
+        if odpoved not in datalist:
+            datalist.append(odpoved)
+            print(receivedString)
+            print(odpoved)
+radio.on_received_string(on_received_string)
--- main.ts
+++ main.ts
@@ -1,18 +1,52 @@
+radio.setGroup(1)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
 let abeceda = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "U", "V", "W", "X", "Y", "Z"]
-let písmeno = abeceda[0]
-basic.showString(písmeno)
-basic.forever(function on_forever() {
-    input.onButtonPressed(Button.A, function on_button_pressed_a() {
-        let písmeno = abeceda[-1]
+let pismeno = 0
+let datalist = [0]
+let start = false
+basic.showString(abeceda[pismeno])
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
+    if (start) {
+        start = false
+        console.log("konec")
+    } else {
+        start = true
+        console.log(start)
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (pismeno > 0) {
+        pismeno -= 1
         basic.clearScreen()
-        basic.showString(písmeno)
-    })
-    input.onButtonPressed(Button.B, function on_button_pressed_b() {
-        let písmeno = abeceda[+1]
+        basic.showString(abeceda[pismeno])
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (pismeno < abeceda.length) {
+        pismeno += 1
         basic.clearScreen()
-        basic.showString(písmeno)
-    })
+        basic.showString(abeceda[pismeno])
+    }
+    
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
-    radio.sendString(písmeno)
+    radio.sendString(abeceda[pismeno])
+})
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    let odpoved = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    if (start) {
+        if (datalist.indexOf(odpoved) < 0) {
+            datalist.push(odpoved)
+            console.log(receivedString)
+            console.log(odpoved)
+        }
+        
+    }
+    
 })
