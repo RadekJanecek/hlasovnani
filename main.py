bb42f0d069c8a9cc956388867c4c371c9ccf564f
Radek Janeček <83546285+RadekJanecek@users.noreply.github.com>
2022-04-20 17:49:27 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-písmeno = 65
-
-
-
-def on_button_pressed_a():
-    global písmeno
-    if písmeno == 65:
-        pass
-    else:
-        písmeno = písmeno - 1
-input.on_button_pressed(Button.A, on_button_pressed_a)
-def on_button_pressed_b():
-    global písmeno
-    if písmeno == 90:
-        pass
-    else:
-        písmeno = písmeno + 1
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
+abeceda = ["A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "U", "V", "W", "X", "Y", "Z"]
+písmeno = abeceda[0]
+basic.show_string(písmeno)
+def on_forever():
+    
+    def on_button_pressed_a():
+        písmeno = abeceda[-1]
+        basic.clear_screen()
+        basic.show_string(písmeno)
+    input.on_button_pressed(Button.A, on_button_pressed_a)
+    def on_button_pressed_b():
+        písmeno = abeceda[+1]
+        basic.clear_screen()
+        basic.show_string(písmeno)
+    input.on_button_pressed(Button.B, on_button_pressed_b)
+basic.forever(on_forever)
 
 def on_logo_event_pressed():
-    print(písmeno)
+    radio.send_string(písmeno)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
--- main.ts
+++ main.ts
@@ -1,22 +1,18 @@
-let písmeno = 65
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    if (písmeno == 65) {
-        
-    } else {
-        písmeno = písmeno - 1
-    }
-    
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    if (písmeno == 90) {
-        
-    } else {
-        písmeno = písmeno + 1
-    }
-    
+let abeceda = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "U", "V", "W", "X", "Y", "Z"]
+let písmeno = abeceda[0]
+basic.showString(písmeno)
+basic.forever(function on_forever() {
+    input.onButtonPressed(Button.A, function on_button_pressed_a() {
+        let písmeno = abeceda[-1]
+        basic.clearScreen()
+        basic.showString(písmeno)
+    })
+    input.onButtonPressed(Button.B, function on_button_pressed_b() {
+        let písmeno = abeceda[+1]
+        basic.clearScreen()
+        basic.showString(písmeno)
+    })
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
-    console.log(písmeno)
+    radio.sendString(písmeno)
 })
